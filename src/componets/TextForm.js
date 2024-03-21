13f700bb4fe44a14fc255026cee2dc54b52880d7
= <=savanyadav377@gmail.com>
2024-03-21 12:52:42 -0800

    [new c]

--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -1,28 +1,50 @@
 import React, { useState } from 'react'
 
 
-const Hey = ()=>{
-    return <button onClick={()=>alert('hey')} className='btn btn-primary ms-4' >Click</button>
+const Hey = () => {
+    return <button onClick={() => alert('hey JS')} className='btn btn-primary ms-4' >Click</button>
 }
 
 export default function TextForm(props) {
     const hendleUpClick = () => {
-        console.log("Uppercase was Clicked");
-        setText("You have clicked on handleUpClick");
+        // console.log("Uppercase was Clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
     }
- 
+
+    const hendleLoClick = () => {
+        // console.log("Uppercase was Clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const hendleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here');
+
+
+    const [text, setText] = useState('');
     return (
-        <div className="mb-3">
-            <h4>{props.heading}</h4>
-            <textarea className="form-control" value={text} onChange={hendleOnChange} id="myBox" rows="8"></textarea>
-            <button className="btn btn-primary mt-3" onClick={hendleUpClick}>Convert to Upercase</button>
-            <Hey />
-        </div>
+        <>
+            <div className="mb-3 container">
+                <h4 className='mt-4'>{props.heading}</h4>
+                <textarea className="form-control" value={text} onChange={hendleOnChange} id="myBox" rows="10"></textarea>
+                <button className="btn btn-primary mt-3" onClick={hendleUpClick}>Convert to Upercase</button>
+                <button className="btn btn-primary mt-3 ms-4" onClick={hendleLoClick}>Convert to Lowercase</button>
+                {/* <Hey /> */}
+            </div>
+
+            <div className="container">
+                <h4>Your text summary</h4>
+                <p>  {text.split(" ").length} and {text.length} characters</p>
+                <p>  {0.008 * text.split(" ").length} Minutes read</p>
+
+
+                <h4>Preview</h4>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
 
